9a7e3bb6e5b19964954169e02875ba28a936314f
Daniel <daniel.seidel.work@gmail.com>
2018-04-22 14:24:17 +0200

    implemented scale
    + scale to display how much is how much
    + auto refreshing scale on zoom
    + auto adjusting scale-meter

--- Gravity Sim/src/window/DrawComp.java
+++ Gravity Sim/src/window/DrawComp.java
@@ -1,4 +1,5 @@
 package window;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -7,6 +8,7 @@ import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
 
 import main.Controller;
 import main.Main;
@@ -65,15 +67,18 @@ public class DrawComp extends JComponent{
 		
 		this.applyMouseDrag();
 		
+		drawEllipseWithFocusPoints(g2);
+		
 		int radiusPix;
 		if(bodies != null)
 			for(Body b : bodies) {
 				radiusPix = (int) (b.radiusMeters / this.pxInMeters);
 				g2.drawOval(cameraOffsetXPix + (int)(b.x/pxInMeters) - radiusPix, cameraOffsetYPix + (int)(b.y/pxInMeters) - radiusPix, radiusPix*2, radiusPix*2);
-				drawInfoTab(g2, b);
+//				drawInfoTab(g2, b);
 			}
 		
-		drawEllipseWithFocusPoints(g2);
+		
+		this.drawScale(g2);
 		
 		lastWidth = this.getWidth();
 		lastHeight = this.getHeight();
@@ -112,6 +117,8 @@ public class DrawComp extends JComponent{
 	}
 	
 	private void drawEllipse(Graphics2D g2, double aPix, double bPix, double degreeRadians, Body bodyCenter, double centerX, double centerY) {
+		
+		g2.setColor(Color.GRAY);
 		
 		double schrittweite = 2;
 		int wdh = (int)(4*aPix);
@@ -186,35 +193,108 @@ public class DrawComp extends JComponent{
 	    g2.drawLine(pointCacheX, pointCacheY, pointStartX, pointStartY);
 	}
 
-	private void drawInfoTab(Graphics2D g2, Body body) {
+	
+	private final int scaleOffsetX = 50;
+	private final int scaleOffsetY = 50;
+	private final int scaleBarThickness = 5;
+	private volatile String scaleText;
+	private volatile int scaleLengthPix;
+	private void recalcScale(Graphics2D g2) {
+		
+		////calculate scale line length
+		
+		double scalePix = 1 / this.pxInMeters;
+		int scalePixMin = 40;
+		while(true) {
+			
+			//if scale is too small, increase it
+			if(scalePix < scalePixMin)
+				scalePix *= 5;
+			else
+				break;
+			
+			//if scale is too small, increase it
+			if(scalePix < scalePixMin)
+				scalePix *= 2;
+			else
+				break;			
+		}
+		this.scaleLengthPix = (int) scalePix;		
 		
 		
+		////calculate scale text
+		double scaleMeters = scalePix * this.pxInMeters;
+		String scaleRaw = String.valueOf(scaleMeters);
 		
+		//first number:
+		String firstNumber;
+		int zeros;
+		if(scaleRaw.startsWith("4") || scaleRaw.startsWith("5"))
+			firstNumber = "5";			
+		else 
+			firstNumber = "1";
+				
+		
+		//amount of zeros:
+		zeros = scaleRaw.indexOf(".") - 1;
+		
+		if(scaleRaw.startsWith("9"))
+			zeros++;
+		
+		//unit:
+		int exp = zeros;
+		String[] scaleRawSplit = scaleRaw.split("E");
+		if(scaleRawSplit.length == 2)
+			exp += Integer.valueOf(scaleRawSplit[1]);
+		
+		//System.out.println("exp only calc: " + (int) Math.log10(scaleMeters));
+		
+		
+		String scaleUnit;		
+		if(exp < 3) {
+			scaleUnit = " m";
+		}else if(exp < 6) {
+			scaleUnit = " km";
+		}else if(exp < 9) {
+			scaleUnit = " 000 km";
+		}else if(exp < 12) {
+			scaleUnit = " Mio km";
+		}else if(exp < 15) {
+			scaleUnit = " Mrd km";
+		}else if(exp < 18) {
+			scaleUnit = " Billio km";
+		}else if(exp < 21) {
+			scaleUnit = " Billia km";
+		}else {
+			scaleUnit = " too big to display xD";
+		}
+		
+		//append everything
+		String scaleText = firstNumber;
+		for(int i = 0; i < exp % 3; i++)
+			scaleText += "0";
+		scaleText += scaleUnit;
+		
+		this.scaleText = scaleText;	
+	
 	}
 	
-//	private final int skalaOffsetX = 
-//	private void drawSkala(Graphics2D g2) {
-//		
-//		double[] zoomLevels = new double[] 
-//				{1E8, 1E9, 1E10};
-//		
-//	}
+	private void drawScale(Graphics2D g2) {
+		if(scaleText == null)
+			recalcScale(g2);
+		
+		g2.drawRect(this.getWidth() - scaleOffsetX - (int) this.scaleLengthPix, this.getHeight() - scaleOffsetY - this.scaleBarThickness, (int) this.scaleLengthPix, this.scaleBarThickness);
+		
+		g2.drawString(
+				this.scaleText, 
+				this.getWidth() - this.scaleOffsetX - SwingUtilities.computeStringWidth(g2.getFontMetrics(), this.scaleText),
+				this.getHeight() - this.scaleOffsetY - this.scaleBarThickness - g2.getFont().getSize()
+			);
+		
+	}
 	
 //--------------------------------------------------------------------------------------------------
 	//Logic
-	private double zoomStrength = 0.8;
-	public void zoom(boolean in, int xPix, int yPix) {
-		
-		Point2D.Double point = getPositionOnCoordinateSystem(xPix, yPix);
-		if(in) {
-			this.pxInMeters *= zoomStrength;
-		}else {
-			this.pxInMeters /= zoomStrength;
-		}
-		
-		this.positionPointAt(point.getX(), point.getY(), xPix, yPix);		
-	}
-	
 	public void centerCamera() {
 		cameraOffsetXPix = this.getWidth()/2;
 		cameraOffsetYPix = this.getHeight()/2;
@@ -260,6 +340,24 @@ public class DrawComp extends JComponent{
 	
 //--------------------------------------------------------------------------------------------------
 	//MOVEMENT
+	private double zoomStrength = 0.9D;
+	public void zoom(boolean in, int xPix, int yPix) {
+		
+		Point2D.Double point = getPositionOnCoordinateSystem(xPix, yPix);
+		if(in) {
+			this.pxInMeters *= zoomStrength;
+		}else {
+			this.pxInMeters /= zoomStrength;
+		}
+		
+		if(this.pxInMeters < 1)
+			this.pxInMeters = 1;
+		
+		this.positionPointAt(point.getX(), point.getY(), xPix, yPix);
+		
+		this.recalcScale((Graphics2D) this.getGraphics());
+	}
+	
 	
 	private volatile boolean currentlyMouseDragging = false;
 	private double mouseDragStartMetersX;
@@ -280,7 +378,8 @@ public class DrawComp extends JComponent{
 			return;
 		
 		Point mousePos = this.getMousePosition();
-		this.positionPointAt(this.mouseDragStartMetersX, this.mouseDragStartMetersY, mousePos.x, mousePos.y);
+		if(mousePos != null)
+			this.positionPointAt(this.mouseDragStartMetersX, this.mouseDragStartMetersY, mousePos.x, mousePos.y);
 	}
 	
 //--------------------------------------------------------------------------------------------------
